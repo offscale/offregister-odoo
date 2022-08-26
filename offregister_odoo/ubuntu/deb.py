5abaa3444136c420d2345bfb1fde32da73192456
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-08-25 22:06:30 -0400

    [*.py] Update to threadsafe Python 3 compatible Fabric ; [README.md] Update support Python version range ; [LICENSE-{APACHE,MIT}] Update copyright year range

--- offregister_odoo/ubuntu/deb.py
+++ offregister_odoo/ubuntu/deb.py
@@ -1,27 +1,26 @@
 from os import path
 
-from fabric.contrib.files import append, exists, upload_template
-from fabric.operations import sudo
+from fabric.contrib.files import append, exists
 from offregister_fab_utils.apt import apt_depends, is_installed
 from offregister_fab_utils.ubuntu.systemd import restart_systemd
 from pkg_resources import resource_filename
 
 
-def install0(*args, **kwargs):
-    if len(is_installed("odoo")) == 0:
+def install0(c, *args, **kwargs):
+    if len(is_installed(c, "odoo")) == 0:
         return "already installed odoo"
 
-    if not exists("/etc/apt/sources.list.d/odoo.list"):
-        sudo("wget -O - https://nightly.odoo.com/odoo.key | apt-key add -")
+    if not exists(c, runner=c.run, path="/etc/apt/sources.list.d/odoo.list"):
+        c.sudo("wget -O - https://nightly.odoo.com/odoo.key | apt-key add -")
         append(
             "/etc/apt/sources.list.d/odoo.list",
             "deb http://nightly.odoo.com/11.0/nightly/deb/ ./",
             use_sudo=True,
         )
 
-    apt_depends("postgresql", "odoo", "libldap2-dev", "libsasl2-dev")
-    sudo("pip3 install pip==9.0.3")
-    sudo("pip3 install vobject qrcode pyldap num2words")
+    apt_depends(c, "postgresql", "odoo", "libldap2-dev", "libsasl2-dev")
+    c.sudo("pip3 install pip==9.0.3")
+    c.sudo("pip3 install vobject qrcode pyldap num2words")
 
     return "installed odoo"
 
@@ -31,7 +30,8 @@ def configure1(*args, **kwargs):
     if not server_name:
         raise TypeError("SERVER_NAME or DNS_NAME must be specified")
 
-    upload_template(
+    upload_template_fmt(
+        c,
         resource_filename(
             "offregister_odoo", path.join("conf", "nginx-sites-available.conf")
         ),
--- offregister_odoo/ubuntu/docker.py
+++ offregister_odoo/ubuntu/docker.py
@@ -1,8 +1,8 @@
 from os import path
 from random import randint
 
-from fabric.contrib.files import append, upload_template
-from fabric.operations import prompt, run, sudo
+from fabric.contrib.files import append
+from fabric.operations import prompt
 from offregister_fab_utils.apt import apt_depends, get_pretty_name
 from offutils import gen_random_str
 from pkg_resources import resource_filename
@@ -15,8 +15,8 @@ def install_docker0(*args, **kwargs):
     if not dist == "precise":
         raise NotImplementedError("Remote OS is not {}".format(dist))
 
-    apt_depends("apt-transport-https", "ca-certificates")
-    sudo(
+    apt_depends(c, "apt-transport-https", "ca-certificates")
+    c.sudo(
         "apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 "
         "--recv-keys 58118E89F3A912897C070ADBF76221572C52609D"
     )
@@ -26,34 +26,34 @@ def install_docker0(*args, **kwargs):
         use_sudo=True,
     )
 
-    docker_running = run("status docker", quiet=True, warn_only=True).succeeded
+    docker_running = c.run("status docker", hide=True, warn=True).exited == 0
     if not docker_running and dist == "precise":
-        apt_depends("linux-image-generic-lts-trusty")
+        apt_depends(c, "linux-image-generic-lts-trusty")
         if (lambda reboot: reboot.lower() in frozenset(("y", "yes")))(
             prompt("Reboot? [y|n]")
         ):
-            sudo("reboot")
-        apt_depends("docker-engine")  # Reboot for this
+            c.sudo("reboot")
+        apt_depends(c, "docker-engine")  # Reboot for this
 
         # Preferably never reach these two lines
-        sudo("service docker start")
-        sudo("docker run hello-world")
+        c.sudo("service docker start")
+        c.sudo("docker run hello-world")
 
-    if run("grep -q docker /etc/group", quiet=True, warn_only=True).failed:
-        sudo("groupadd docker")
-        sudo("usermod -aG docker $USER")
-        sudo("docker run hello-world")
+    if c.run("grep -q docker /etc/group", hide=True, warn=True).exited != 0:
+        c.sudo("groupadd docker")
+        c.sudo("usermod -aG docker $USER")
+        c.sudo("docker run hello-world")
 
 
 def _destroy_docker_container(name, destroy=None):
-    container_id = run(
+    container_id = c.run(
         'docker ps -a --format "{{.ID}}" -f' + "name={name}".format(name=name)
     )
     if len(container_id) > 15:
         raise NotImplementedError("Support for multiple containers of same name")
 
     if container_id:
-        run("docker stop {container_id}".format(container_id=container_id))
+        c.run("docker stop {container_id}".format(container_id=container_id))
         if (
             destroy is False
             or destroy is None
@@ -63,13 +63,15 @@ def _destroy_docker_container(name, destroy=None):
         ):
             return "Did not destroy {name}.".format(name=name)
 
-        return run("docker rm --force {container_id}".format(container_id=container_id))
+        return c.run(
+            "docker rm --force {container_id}".format(container_id=container_id)
+        )
 
 
 def setup_postgres1(*args, **kwargs):
     _destroy_docker_container("odoo", destroy=True)
     _destroy_docker_container("db", destroy=True)
-    run(
+    c.run(
         "docker run -d -e POSTGRES_USER={user} -e POSTGRES_PASSWORD={password} --name db postgres:9.4".format(
             user=gen_random_str(randint(10, 20)),
             password=gen_random_str(randint(10, 20)),
@@ -78,16 +80,19 @@ def setup_postgres1(*args, **kwargs):
 
 
 def setup_odoo2(*args, **kwargs):
-    sudo("stop teamcity && rm -rf /etc/init/teamcity.conf", quiet=True, warn_only=True)
+    c.sudo("stop teamcity && rm -rf /etc/init/teamcity.conf", hide=True, warn=True)
     _destroy_docker_container("odoo", destroy=True)
     for fname in ("stdout", "stderr"):
-        sudo("touch /var/log/upstart/odoo.{fname}.log".format(fname=fname))
-        sudo("chown $USER:$GROUP /var/log/upstart/odoo.{fname}.log".format(fname=fname))
-    upload_template(
+        c.sudo("touch /var/log/upstart/odoo.{fname}.log".format(fname=fname))
+        c.sudo(
+            "chown $USER:$GROUP /var/log/upstart/odoo.{fname}.log".format(fname=fname)
+        )
+    upload_template_fmt(
+        c,
         resource_filename("offregister_odoo", path.join("conf", "odoo.conf")),
         "/etc/init/odoo.conf",
         use_sudo=True,
-        context={"USER": run("echo $USER"), "AUTHOR": __author__},
+        context={"USER": c.run("echo $USER").stdout.rstrip(), "AUTHOR": __author__},
     )
-    if run("status odoo", quiet=True, warn_only=True).failed:
-        sudo("start odoo")
+    if c.run("status odoo", hide=True, warn=True).exited != 0:
+        c.sudo("start odoo")
